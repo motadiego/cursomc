0082b5220cab8e696d422f455df99f1750b265ea
Diego Mota <diegoalves123@gmail.com>
2019-02-12 20:01:21 -0200

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/diegomota/cursomc/domain/enums/EstadoPagamento.java
+++ src/main/java/com/diegomota/cursomc/domain/enums/EstadoPagamento.java
@@ -3,7 +3,7 @@ package com.diegomota.cursomc.domain.enums;
 public enum EstadoPagamento {
 	
 	
-	PENDENTE(1 , "Pendenre"),
+	PENDENTE(1 , "Pendente"),
 	QUITADO(2 , "Quitado"),
 	CANCELADO(3 , "Cancelado");
 	
---
+++ src/main/java/com/diegomota/cursomc/services/PedidoService.java
@@ -0,0 +1,28 @@
+package com.diegomota.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegomota.cursomc.domain.Pedido;
+import com.diegomota.cursomc.repositories.PedidoRepository;
+import com.diegomota.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido cat =  repo.findOne(id);
+		
+		if(cat == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		
+		
+		return cat;
+	}
+	
+}
