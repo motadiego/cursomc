dd0ec0d6b28faa313831077c956ecb50779c9692
Diego Mota <diegoalves123@gmail.com>
2019-02-26 22:59:36 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/diegomota/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.diegomota.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.diegomota.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	
+	@Bean
+	public boolean instatiateDataBase() {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+	
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+	
+}
