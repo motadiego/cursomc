b5b74d63cfb5346c2651373fa9bf4f804b2744e1
Diego Mota <diegoalves123@gmail.com>
2019-03-19 22:57:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/diegomota/cursomc/CursomcApplication.java
+++ src/main/java/com/diegomota/cursomc/CursomcApplication.java
@@ -1,13 +1,18 @@
 package com.diegomota.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import com.diegomota.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication extends SpringBootServletInitializer  implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -15,15 +20,10 @@ public class CursomcApplication extends SpringBootServletInitializer  implements
 	}
 	
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CursomcApplication.class);
-	}
 	
-
 	@Override
 	public void run(String... args) throws Exception {
-	
+		s3Service.uploadFile("C:\\Users\\Diego Mota\\Desktop\\Curso Spring\\workspace\\cursomc\\src\\arquivos\\fotos\\computador.jpg");
 	}
 
 }
