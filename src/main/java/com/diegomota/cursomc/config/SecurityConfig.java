cb953d878b6cb77d567e20bbf3db411a5436db38
Diego Mota <diegoalves123@gmail.com>
2019-04-04 19:56:41 -0300

    Seção: 9 Aplicação Ionic - Parte 1/2 132. Salvando novo cliente
    Foi identificado que ao chamar o serviço de cadastrar um novo cliente
    via post /clientes , estava retornando acesso negado.

--- src/main/java/com/diegomota/cursomc/config/SecurityConfig.java
+++ src/main/java/com/diegomota/cursomc/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 	
 	private static final String[] PUBLIC_MATCHES_POST = {
-			"/clientes/",
+			"/clientes/**",
 			"/auth/forgot/**"
 	};
 	
